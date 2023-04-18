8da8d511440a6f3b664c0c6c61f7d7e97f0f22fd
wookjinnoh <dnrwls4191@gmail.com>
2023-04-19 01:29:46 +0900

    refactor: 함수형 업데이트

--- src/App.js
+++ src/App.js
@@ -4,51 +4,44 @@ import Main from './components/Main';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'test1',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: 'test2',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 3; i++) {
+    array.push({
+      id: i,
+      text: `TEST - ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
 
-  const nextId = useRef(3);
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
 
-  const onRemove = useCallback(
-    (id) => {
-      // id가 같으면 todos 배열에서 나가고, 다르면 todos 유지, filter는 남기는 것
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const nextId = useRef(4);
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    // 함수형 업데이트
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    // id가 같으면 todos 배열에서 나가고, 다르면 todos 유지, filter는 남기는 것
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
   return (
     <div className="App">
