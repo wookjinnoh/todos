e8b8010f8ccce2cbf913813a493a9bdeab8a8cf8
wookjinnoh <dnrwls4191@gmail.com>
2023-04-18 23:02:45 +0900

    feat: Todos 리스트 나열 기능 완료

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// components
+import TodoListItem from './TodoListItem';
+// styles
+import '../styles/TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
